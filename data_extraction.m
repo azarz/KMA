991a2dfe12c9f9b93e35e001db4b87a3c3cad564
Ogus <ogustin.gagnon@gmail.com>
2016-11-30 11:51:39 +0100

    New method to extract data

---
+++ data_extraction.m
@@ -0,0 +1,18 @@
+% 
+% This method is used to extract data from images,
+% and store them as variable to later save them as .mat files
+% 
+
+clear; clc; close all;
+addpath(genpath('../'));
+
+disp('start');
+
+[X,Y,U] = extract('1966_reduced_crop2','1966_vt2',1);
+
+[X,Y,~,~,~] = ppc(X,Y,0.005);
+s = size(U); n = mean(0.05*s(1));
+disp(n);
+U = U(1:n,:);
+
+disp('finished');
